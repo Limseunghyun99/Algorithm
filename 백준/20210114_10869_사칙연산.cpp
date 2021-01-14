5ed3d37f2b6bf095690becdbf089371244bcf5de
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2021-01-14 21:15:16 +0900

    Create 20210114_10869_사칙연산.cpp

---
+++ 백준/20210114_10869_사칙연산.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<(int)a%b<<endl;
+    return 0;
+}
