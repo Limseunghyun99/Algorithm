34c39b60fba0126fd8e8b9a933400ee393f966a1
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2021-02-07 20:56:52 +0900

    Create 20210207_11022_A+B-8.cpp

---
+++ 백준/20210207_11022_A+B-8.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int a,b;
+    scanf("%d",&num);
+    
+    for (int i=1;i<=num;i++){
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n",i, a, b, a+b);
+    }
+    return 0; 
+}
