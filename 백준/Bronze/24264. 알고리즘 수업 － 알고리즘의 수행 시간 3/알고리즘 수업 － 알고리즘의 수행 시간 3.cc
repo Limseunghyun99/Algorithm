f47e28e4c696192c0af87cb862c85e546b5ce8c1
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2024-08-28 01:33:22 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    long long int n;
+ 
+    cin >> n; //C언어 : scanf("%d",&n);
+ 
+    cout << n*n << '\n' << 2 << '\n'; //C언어 : printf("%d\n%d", n*n, 2);
+ 
+    return 0;
+}
