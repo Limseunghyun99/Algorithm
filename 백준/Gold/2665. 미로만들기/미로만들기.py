8cf1276c7a075b2b2b63b3fa3e44a02ca62a0d5f
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2025-02-25 20:25:20 +0900

    [Gold IV] Title: 미로만들기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,35 +1,34 @@
-# 미로만들기
 from collections import deque
-import sys
-input = sys.stdin.readline
 N = int(input())
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
 arr = []
-for i in range(N):
-    arr.append(list(map(int, input().strip())))
-
+for _ in range(N):    arr.append(list(map(int, input().strip())))
 
 def bfs():
     q = deque()
-    q.append((0, 0))
+    q.append((0,0))
+    # NxN
     visited = [[-1] * N for _ in range(N)]
-    visited[0][0] = 0
-    while q:
-        x, y = q.popleft()
-        if x == N-1 and y == N-1:
-            return visited[x][y]
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == -1:
-                if arr[nx][ny] == 1:
-                    q.appendleft((nx, ny))
-                    visited[nx][ny] = visited[x][y]
-                else:
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
 
+    visited[0][0]=0
+    while q:
+        x,y = q.popleft()
+        if x == N-1 and y == N-1:   return visited[x][y]
+        for k in range(4):
+            cur_x = x+dx[k]
+            cur_y = y + dy[k]
+
+            # 좌표가 유효하고 아직 방문 안했다면
+            if 0<=cur_x<N and 0<=cur_y <N and visited[cur_x][cur_y] == -1:
+                # 흰방
+                if arr[cur_x][cur_y] == 1:
+                    q.appendleft((cur_x,cur_y))
+                    visited[cur_x][cur_y] = visited[x][y]
+                # 검은방방
+                else:
+                    q.append((cur_x,cur_y))
+                    visited[cur_x][cur_y] = visited[x][y] +1
 
 print(bfs())
