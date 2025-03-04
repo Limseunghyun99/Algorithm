97f7cef06be69d042deb25d1d2370f2caa6261f5
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2025-03-04 23:55:10 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.py
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.py
@@ -1,3 +1,4 @@
+# 꼼수
 import sys
 input = sys.stdin.readline
 
@@ -6,10 +7,13 @@ save = {}
 save[1] = ["1"]
 save[2] = ["1+1","2"]
 save[3] = ["1+2","1+1+1","2+1","3"]
-for i in range(4,12):
+for i in range(4,11):
     save[i] = []
+    # N보다 1 작은수를 만드는 경우추가
     for j in save[i-1]:    save[i].append(j+"+1")
+    # N보다 2 작은수를 만드는 경우추가
     for j in save[i-2]:    save[i].append(j+"+2")
+    # N보다 3 작은수를 만드는 경우추가
     for j in save[i-3]:    save[i].append(j+"+3")            
 
 if k <= len(save[n]):    print(sorted(save[n])[k-1])
