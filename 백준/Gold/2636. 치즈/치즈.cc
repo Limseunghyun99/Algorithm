5a7898cc455e64d0e06d513c3af4db2586f8b317
Lim_seung_hyun <39540665+Limseunghyun99@users.noreply.github.com>
2025-03-22 00:19:27 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -7,19 +7,13 @@ using namespace std;
 int N, M;
 vector<vector<int>> board;
 vector<vector<bool>> visited;
+queue<pair<int, int>> air_queue;
+queue<pair<int, int>> cheese_queue;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 // 외부 공기와 맞닿은 치즈를 녹이는 BFS
-int bfs_melt_cheese() {
-    queue<pair<int, int>> air_queue;
-    queue<pair<int, int>> cheese_queue;
-    visited.assign(N, vector<bool>(M, false));
-
-    // (0,0)에서 BFS를 시작하여 외부 공기 영역 찾기
-    air_queue.push({0, 0});
-    visited[0][0] = true;
-
+int bfs_melt_cheese() { 
     while (!air_queue.empty()) {
         int x = air_queue.front().first;
         int y = air_queue.front().second;
@@ -81,6 +75,10 @@ int main() {
 
     cin >> N >> M;
     board.assign(N, vector<int>(M));
+		visited.assign(N, vector<bool>(M, false));
+    // (0,0)에서 BFS를 시작하여 외부 공기 영역 찾기
+    air_queue.push({0, 0});
+    visited[0][0] = true;
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
